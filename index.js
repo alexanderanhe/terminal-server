74c9e2e79175704a9d65d0a265495d818077c91a
Alexander Angulo <alex.anguloh@gmail.com>
2021-09-20 00:07:14 -0500

    FEAT: Add redis

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+const redis = require("redis");
+const { promisify } = require("util");
 const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 5000;
-const INDEX = '/index.html';
 const server = express()
-  // .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Server is listening on ${PORT}`));;
 
 const options = {
@@ -20,21 +20,55 @@ const io = socketIO(server, options); // , options
 //   res.send('Hello World!');
 // });
 
+const client = redis.createClient({
+  host: process.env.REDIS_HOST || "localhost",
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PWD || "",
+});
+const redisGetAsync = promisify(client.get).bind(client);
+
+const saveMessage = async (room, message) => {
+  console.log("Saving message", room, message);
+  const data = await redisGetAsync(room);
+
+  // the first message in the chat room
+  if (!data) {
+    console.log("NO DATA ON ROOM", room);
+    return client.set(room, "[]");
+  }
+
+  const json = JSON.parse(data);
+  console.log("FIND DATA", data);
+  json.push(message);
+
+  client.set(room, JSON.stringify(json));
+};
+
 io.on('connection', socket => {
   
   const { id, user: userJson } = socket.handshake.query;
   try {
     const user = JSON.parse(userJson);
     console.log(`A user connected ${user.email}`);
-    socket.on("joinRoom", (room) => {
+  
+    socket.on("joinRoom", async (room) => {
       console.log(`User conected to ${room}`);
       socket.join(room);
+
+      const messageData = await redisGetAsync(room);
+      if (messageData) {
+        // need to make sure that we are grabbing this history data on the frontend
+        socket.to(room).emit("history", messageData);
+      }
     });
   
     socket.on('message', ({room, message}) => {
-      socket.to(room).emit('receive', {
+      const messageContent = {
         sender: userJson, message
-      });
+      };
+      saveMessage(room, messageContent);
+
+      socket.to(room).emit('receive', messageContent);
     });
   
     socket.on("typing", ({ room }) => {
